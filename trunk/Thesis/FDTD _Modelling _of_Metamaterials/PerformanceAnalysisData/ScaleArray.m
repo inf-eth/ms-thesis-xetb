db128119cba16fd03c4d2c825667ad7378480571
infinity.ethereal <infinity.ethereal@aa9d4c98-77a3-73b7-a3d0-b1fda085adb7>
2012-11-27 10:54:50 +0000

    [FDTD Modelling of Metamaterials] - Added ScaleArray.m to scale array. - Added \XAxisLabel and \YAxisLabel to label plots. - Updated GenerateDrawCode.m.

---
+++ trunk/Thesis/FDTD _Modelling _of_Metamaterials/PerformanceAnalysisData/ScaleArray.m
@@ -0,0 +1,11 @@
+% Scaling function. Scales an array.
+% Inputs:
+% 1. Data: Arrays for plotting.
+% 2. Max: Absolute maximum value.
+% 3. Scale: Maximum value.
+function Args = ScaleArray(Data, Max, Scale, Text)
+    Args = '';
+    for i=1:length(Data)
+        Args = sprintf('%s{%2.3gcm/%s}', Args, Scale*Data(i)/Max, int2str(Text(i)));
+    end
+end
